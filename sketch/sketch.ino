31bcd34c2a7a2ff5a1624c34d5eca7f54a145c92
mihkuno <caindayjoeninyo@gmail.com>
2025-01-17 12:15:36 +0800

    removed mq7 sensor

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -3,28 +3,26 @@
 
 #define BLYNK_TEMPLATE_ID      "TMPL60_lVpwjr"
 #define BLYNK_TEMPLATE_NAME    "Gaslynk"
-#define BLYNK_AUTH_TOKEN       "D0y32qZ1nsB7kZIt5pozdGeTXxE_0LQj"
+#define BLYNK_AUTH_TOKEN       "V7qSUHyZU5S_gJdjbjXdKB8XjwWq395J"
 #include <BlynkSimpleEsp32.h>
 
-#define LED_PIN 14 
-#define MQ9_PIN 34
-#define MQ7_PIN 33
-#define MQ2_PIN 32
+#define LED_PIN 32
+#define MQ2_PIN 34
+#define MQ9_PIN 35
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);  // Set the LCD address to 0x27 for a 20 chars and 4 line display
 
-const char* ssid = "PLDTHOMEFIBRdbb60";   // Your Wi-Fi SSID
-const char* password = "PLDTWIFIfbwm9";   // Your Wi-Fi Password
+const char* ssid = "gaslynk";   // Your Wi-Fi SSID
+const char* password = "gaslynk123";   // Your Wi-Fi Password
 
 float mq2_val = 0;
-float mq7_val = 0;
 float mq9_val = 0;
 
 void setup() {
+  
   // Initialize pins and LCD
   pinMode(LED_PIN, OUTPUT);
   pinMode(MQ2_PIN, INPUT);
-  pinMode(MQ7_PIN, INPUT);
   pinMode(MQ9_PIN, INPUT);
 
   lcd.init();                     
@@ -63,16 +61,15 @@ void setup() {
 void loop() {
   // Read analog values from sensors
   mq2_val = analogRead(MQ2_PIN);
-  mq7_val = analogRead(MQ7_PIN);
   mq9_val = analogRead(MQ9_PIN);
 
   // Convert to voltage
   mq2_val *= (3.3 / 4095.0);
-  mq7_val *= (3.3 / 4095.0);
   mq9_val *= (3.3 / 4095.0);
 
   // Turn LED on if any sensor is greater than 1V 
-  if (mq2_val >= 1.0 || mq7_val >= 1.0 || mq9_val >= 1.0) { 
+  // if (mq2_val >= 1.0 || mq7_val >= 1.0 || mq9_val >= 1.0) { 
+  if (mq2_val >= 1.0 || mq9_val >= 1.0) { 
     digitalWrite(LED_PIN, HIGH); 
     // Send alert notification to Blynk
     Blynk.logEvent("gas_alert", "High gas levels detected!");
@@ -82,7 +79,6 @@ void loop() {
 
   // Send data to Blynk
   Blynk.virtualWrite(V0, mq2_val);  // Send MQ2 value to virtual pin V0
-  Blynk.virtualWrite(V1, mq7_val);  // Send MQ7 value to virtual pin V1
   Blynk.virtualWrite(V2, mq9_val);  // Send MQ9 value to virtual pin V2
   Blynk.virtualWrite(V3, digitalRead(LED_PIN));  // Send LED status to virtual pin V3
 
@@ -92,15 +88,10 @@ void loop() {
   lcd.print("MQ2:");
   lcd.setCursor(0, 1);
   lcd.print(String(mq2_val));
-  
+
   lcd.setCursor(6, 0);
-  lcd.print("MQ7:");
-  lcd.setCursor(6, 1);
-  lcd.print(String(mq7_val));
-  
-  lcd.setCursor(12, 0);
   lcd.print("MQ9:");
-  lcd.setCursor(12, 1);
+  lcd.setCursor(6, 1);
   lcd.print(String(mq9_val));
 
   // Run Blynk
